b60396ef830e98eec0e2d54e9d988cc267120bbf
uponup <1030360567@qq.com>
2020-09-28 16:52:01 +0800

    fix 调整Operation

--- MessageBroker/MavlBroker/TopicUtils.swift
+++ MessageBroker/MavlBroker/TopicUtils.swift
@@ -107,3 +107,87 @@ extension Substring {
         return String(self)
     }
 }
+
+
+// MARK: - Operation
+public enum FetchMessagesType: Int {
+    case one = 1
+    case more
+}
+
+enum Operation {
+    case createGroup
+    case oneToOne(_ localId: UInt16, _ toUid: String)
+    case oneToMany(_ localId: UInt16, _ toGid: String)
+    case vitualGroup(_ toGid: String)
+    
+    case joinGroup(_ toGid: String)
+    case quitGroup(_ toGid: String)
+    
+    case uploadToken
+    
+    case fetchMsgs(_ from: String, _ type: FetchMessagesType, _ cursor: String, _ offset: Int)
+    
+    var value: Int {
+        switch self {
+        case .createGroup:  return 0
+        case .oneToOne:     return 1
+        case .oneToMany:    return 2
+        case .vitualGroup:  return 3
+            
+        case .joinGroup:    return 201
+        case .quitGroup:    return 202
+            
+        case .uploadToken:  return 300
+            
+        case .fetchMsgs:    return 401
+        }
+    }
+    
+    
+    var topic: String {
+        let topicPrefix = "\(MavlMessage.shared.appid)/\(value)/\(localId)"
+        
+        switch self {
+        case .createGroup:
+            return "\(topicPrefix)/_"
+        case .oneToOne(_, let uid):
+            return "\(topicPrefix)/\(uid)"
+        case .oneToMany(_, let gid):
+            return "\(topicPrefix)/\(gid)"
+        case .vitualGroup(let gid):
+            return "\(topicPrefix)/\(gid)"
+        case .joinGroup(let gid):
+            return "\(topicPrefix)/\(gid)"
+        case .quitGroup(let gid):
+            return "\(topicPrefix)/\(gid)"
+        case .uploadToken:
+            return "\(topicPrefix)/_"
+        case .fetchMsgs(let from, let type, let cursor, let offset):
+            return "\(topicPrefix)/\(from)/\(type.rawValue)/\(cursor)/\(offset)"
+        }
+    }
+    
+    private var localId: String {
+        switch self {
+        case .createGroup, .vitualGroup, .joinGroup, .quitGroup, .uploadToken, .fetchMsgs:
+            return "0"
+        case .oneToOne(let localId, _):
+            return "\(localId)"
+        case .oneToMany(let localId, _):
+            return "\(localId)"
+        }
+    }
+}
+
+// MARK: OptionalExtension--String
+extension Optional where Wrapped == String {
+    public var value: String {
+        switch self {
+        case .none:
+            return ""
+        case .some(let v):
+            return v;
+        }
+    }
+}
